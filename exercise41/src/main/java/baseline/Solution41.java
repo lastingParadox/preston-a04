ca664fded79c9141c36e510f20b1e06991e5da15
Zander <zanpres@gmail.com>
2021-10-07 09:57:54 -0400

    Initial commit with exercise41 skeleton

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,50 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Zander Preston
+ */
+
+package baseline;
+
+import java.util.List;
+
+public class Solution41 {
+    //Create FileInputStream "in" = to "exercise41_input.txt"
+    //Create FileOutputStream "out" = to "exercise41_output.txt"
+
+    public List<String> createListOfNames() {
+        //Create new BufferedReader "reader"
+        //String "line" is set equal to br.readLine()
+        //Create new list "names"
+        //While line is not null:
+            //Adds the line to names
+        //Returns names
+        return null;
+    }
+
+    public List<String> sortNames(List<String> names) {
+        //Use Collections.sort to sort the values in names
+        //returns names
+        return null;
+    }
+
+    public String listOutput(List<String> names) {
+        //Create new StringBuilder "output"
+        //Append "Total of 'names.length' names\n" to output
+        //Append "-----------------" to output
+        //For each name in names:
+            //Append "\nname" to output
+        //Return String value of output
+        return "";
+    }
+
+    public static void main(String[] arg) {
+        //Create new instance of Solution41 "sorter"
+
+        //List "names" is equal to sorter.createArrayOfNames()
+        //names is equal to sorter.sortNames()
+
+        //String "output" is equal to sorter.listOutput()
+        //Print output to terminal
+        //Write output to "exercise41_output.txt"
+    }
+}
