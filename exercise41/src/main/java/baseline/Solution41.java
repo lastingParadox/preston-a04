2c7f26d424aa129d13fcc05c315cccaf5ba57ab4
Zander <zanpres@gmail.com>
2021-10-09 18:45:28 -0400

    Updated diagram, added method logic and unit tests to exercise41

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -5,39 +5,49 @@
 
 package baseline;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Solution41 {
-    //Create FileInputStream "in" = to "exercise41_input.txt"
-    //Create FileOutputStream "out" = to "exercise41_output.txt"
 
-    public List<String> createListOfNames() {
-        //Create new BufferedReader "reader"
-        //String "line" is set equal to br.readLine()
-        //Create new list "names"
-        //While line is not null:
-            //Adds the line to names
-        //Returns names
-        return null;
+    public List<String> createListOfNames() throws IOException {
+        //Creates and returns a list of names from the exercise41_input.txt file.
+        List<String> nameList = new ArrayList<>();
+        //Tries to read the lines, provided the file exists.
+        try (BufferedReader reader = new BufferedReader(new FileReader("../exercise41/data/exercise41_input.txt"))) {
+            String line = reader.readLine();
+            while (line != null) {
+                nameList.add(line);
+                line = reader.readLine();
+            }
+        }
+        return nameList;
     }
 
     public List<String> sortNames(List<String> names) {
-        //Use Collections.sort to sort the values in names
-        //returns names
-        return null;
+        //Sorts the names in the list using the Collections.sort method
+        Collections.sort(names);
+        return names;
     }
 
     public String listOutput(List<String> names) {
-        //Create new StringBuilder "output"
-        //Append "Total of 'names.length' names\n" to output
-        //Append "-----------------" to output
-        //For each name in names:
-            //Append "\nname" to output
-        //Return String value of output
-        return "";
+        //Returns a string representing the table of sorted names.
+        StringBuilder output = new StringBuilder();
+        output.append(String.format("Total of %d names%n", names.size()));
+        output.append("-----------------%n");
+        for (String name : names) {
+            output.append(String.format("%n%s", name));
+        }
+        return String.valueOf(output);
     }
 
     public static void main(String[] arg) {
+
         //Create new instance of Solution41 "sorter"
 
         //List "names" is equal to sorter.createArrayOfNames()
