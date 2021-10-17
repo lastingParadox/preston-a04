378d8379f577ef78281e88aa507081b226dd45a5
lastingParadox <zanpres@gmail.com>
2021-10-17 11:38:16 -0400

    Created method logic and unit tests in exercise46

--- exercise46/src/main/java/baseline/FrequencySorter.java
+++ exercise46/src/main/java/baseline/FrequencySorter.java
@@ -5,50 +5,61 @@
 
 package baseline;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.List;
-import java.util.Map;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class FrequencySorter {
 
-    //private File inputFile
+    private final File inputFile;
 
     FrequencySorter(File inputFile) {
-        //this inputFile = inputFile
+        this.inputFile = inputFile;
     }
 
     public Map<String, Integer> createFrequencyMap() {
-        //Create Map of String keys and integer values "wordMap"
-        //try to reader inputFile with a BufferedReader "reader"
-            //String line = reader.readLine()
-            //while line is not null:
-                //Array of strings "temp" is equal to line.split with delimiter of a space
+        //Tries to read the file provided and creates a map of how many times each word is used in the file.
+        Map<String, Integer> wordMap = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            String line = reader.readLine();
+            //Reads each line and adds to the counter for each word in the line.
+            while (line != null) {
+                String[] temp = line.split(" ");
 
-                //Loop for each word in temp:
-                    //If wordMap contains the word as a key:
-                        //Increase that key's value by one.
-                    //Else:
-                        //Create a key as the word and set it to one.
+                for (String word : temp) {
+                    if (wordMap.containsKey(word)) {
+                        wordMap.put(word, 1 + wordMap.get(word));
+                    }
+                    else {
+                        wordMap.put(word, 1);
+                    }
+                }
 
-                //line = reader.readline()
-        //return wordMap
-        return null;
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return wordMap;
     }
 
     public List<Map.Entry<String, Integer>> sortedMapList(Map<String, Integer> wordMap) {
-        //List of Map entries of String keys and integer values "list" is a linked list of wordMap entries.
-        //Sort list by Map.Entry.comparingByValue()
-        //Reverse the list.
-        //return the list.
-        return null;
+        //Sorts the data in the map into a list in descending order of value.
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(wordMap.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        Collections.reverse(list);
+        return list;
     }
 
     public String histogram(List<Map.Entry<String, Integer>> list) {
-        //StringBuilder output is created
-        //For each entry in the list:
-            //String star is an asterisk set to repeat entry.getValue number of times.
-            //Append "'entry.getKey': 'star'"
-        //return String value of output
-        return null;
+        //Creates a histogram of the data in the list provided and returns it as a string.
+        StringBuilder output = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : list) {
+            String star = "*".repeat(Math.max(0, entry.getValue()));
+            output.append(String.format("%s: %s%n", entry.getKey(), star));
+        }
+        return String.valueOf(output);
     }
 }
